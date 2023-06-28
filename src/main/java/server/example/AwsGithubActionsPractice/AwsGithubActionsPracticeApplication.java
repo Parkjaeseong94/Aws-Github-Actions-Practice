9b4353a0b61af060e7b69636b30157819d529f76
Parkjaeseong94 <wotjddml159@gmail.com>
2023-06-28 22:01:33 +0900

    dd

---
+++ src/main/java/server/example/AwsGithubActionsPractice/AwsGithubActionsPracticeApplication.java
@@ -0,0 +1,13 @@
+package server.example.AwsGithubActionsPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsGithubActionsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsGithubActionsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/example/AwsGithubActionsPractice/AwsGithubActionsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package server.example.AwsGithubActionsPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsGithubActionsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
