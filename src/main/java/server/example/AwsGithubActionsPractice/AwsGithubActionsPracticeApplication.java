faf62a84f40f8dbe964ca948cc2f7ed647ce6c2a
Parkjaeseong94 <wotjddml159@gmail.com>
2023-06-29 12:12:52 +0900

    hello world!

--- src/main/java/server/example/AwsGithubActionsPractice/AwsGithubActionsPracticeApplication.java
+++ src/main/java/server/example/AwsGithubActionsPractice/AwsGithubActionsPracticeApplication.java
@@ -2,10 +2,18 @@ package server.example.AwsGithubActionsPractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AwsGithubActionsPracticeApplication {
 
+	@RequestMapping("/")
+	String home(){
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AwsGithubActionsPracticeApplication.class, args);
 	}
