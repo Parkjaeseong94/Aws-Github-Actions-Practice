4509d41e89fbde32fef1073f66dcee5892631156
Parkjaeseong94 <wotjddml159@gmail.com>
2023-06-29 11:40:21 +0900

    gradle.yml 수정

--- src/main/java/server/example/AwsGithubActionsPractice/Hellocontroller.java
+++ src/main/java/server/example/AwsGithubActionsPractice/Hellocontroller.java
@@ -2,10 +2,12 @@ package server.example.AwsGithubActionsPractice;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class Hellocontroller {
-    @GetMapping("/")
+    @GetMapping
     public String hello(){
         String hi = "hello";
         return hi;
